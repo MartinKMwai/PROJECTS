1d5df3e987c4e474e9bbedb97722f7e0626fa731
MartinKMwai <martinkibirumwai@gmail.com>
2024-02-05 06:01:42 +0800

    Depposit function created and completed.

--- SLOT MACHINE/main.py
+++ SLOT MACHINE/main.py
@@ -2,5 +2,16 @@
 
 def deposit():
     while True:
-        pass
+        amount = input ("How much money fo you want to deposit? $  ")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Please input an amount greater than 0. ")
+        
+        else:
+            print("Please input amount in number format. ")
+
+    return amount
 
