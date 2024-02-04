1163f4a2f933a5a9e1cf16a0c4b5e61c5e33f515
MartinKMwai <martinkibirumwai@gmail.com>
2024-02-05 06:31:28 +0800

    Few bugs fixed

--- SLOT MACHINE/main.py
+++ SLOT MACHINE/main.py
@@ -2,12 +2,14 @@
 
 
 MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
 
 
 
 def deposit():
     while True:
-        amount = input ("How much money fo you want to deposit? $")
+        amount = input ("How much money do you want to deposit? $")
         if amount.isdigit():
             amount = int(amount)
             if amount > 0:
@@ -21,7 +23,7 @@ def deposit():
     return amount
 
 
-def get_number_of_betting_lines(amount):
+def get_number_of_betting_lines():
     while True:
         lines = input ("How many lines do you want to bet on? ")
         if lines.isdigit():
@@ -29,17 +31,20 @@ def get_number_of_betting_lines(amount):
             if lines >= 1 and lines <= MAX_LINES:
                 break
             else:
-                print ("Please input lines between one and " + str(MAX_LINES) + " inclusive. ")
+                print ("Please input lines between 1 and " + str(MAX_LINES) + " inclusive. ")
         else:
             print ("Please input your number of lines in number format. ")
 
     return lines
 
+#def amount_per_line(lines):
 
 
    
 def main():
     balance = deposit()
+    lines = get_number_of_betting_lines()
+    print (balance, lines)
 
 
 
