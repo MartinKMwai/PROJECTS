5c2468df4663083ce7ab21e38e64af27f03a8dba
MartinKMwai <martinkibirumwai@gmail.com>
2024-02-06 06:23:11 +0800

    "amount_per_line" function defined

--- SLOT MACHINE/main.py
+++ SLOT MACHINE/main.py
@@ -37,14 +37,35 @@ def get_number_of_betting_lines():
 
     return lines
 
-#def amount_per_line(lines):
-
+def amount_per_line(lines, amount):
+    while True:
+        bet_per_line = input ("How much would you like to bet per line? $")
+        if bet_per_line.isdigit():
+            bet_per_line  = int(bet_per_line )
+            if bet_per_line  >= MIN_BET and bet_per_line  <= MAX_BET:
+                betting_amount = bet_per_line  *lines
+                if betting_amount <= amount:
+                    print ("Your total betting amount is $" + str(betting_amount))
+                    break
+                else:
+                    print ("Your betting balance is " + str(amount) + " and you want to bet $" + str(betting_amount))
+                    print ("You do not have money for that bet, bet lesser money per line")
+            else:
+                print ("Please input a betting amount between $" + str(MIN_BET) + " and $" + str(MAX_BET) + "." ) 
+        else:
+            print ("Please input the bet that you want to mske in number format.")
+    return bet_per_line, betting_amount
 
    
 def main():
-    balance = deposit()
+    amount = deposit()
     lines = get_number_of_betting_lines()
-    print (balance, lines)
+    bet_per_line, betting_amount = amount_per_line(lines, amount)
+    print (amount, lines)
+    print (f"Your balance is {amount}")
+    print (f"Number of lines : {lines}")
+    print (f"Your bet per line: ${bet_per_line}")
+    print (f"Your total betting amount is ${betting_amount} ")
 
 
 
