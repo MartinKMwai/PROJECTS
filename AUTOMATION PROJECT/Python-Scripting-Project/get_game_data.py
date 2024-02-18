69829d9ce54f2fad42c5bc1ecf87e2a6ad8fed9a
MartinKMwai <martinkibirumwai@gmail.com>
2024-02-19 06:10:40 +0800

    Defining of paths in the main function, done.

--- AUTOMATION PROJECT/Python-Scripting-Project/get_game_data.py
+++ AUTOMATION PROJECT/Python-Scripting-Project/get_game_data.py
@@ -6,6 +6,15 @@ import shutil #copy and overwrite on files. File operationms here
 from subprocess import PIPE, run #run any terminal commands that we want
 import sys #access to cmd args
 
+def main(source, target):
+    current_working_directory = os.getcwd()
+    source_path = os.path.join(current_working_directory, source)
+    #do not use string concatenations since rthe path dividwers are different depending on the os
+    target_path = os.path.join(current_working_directory, target)
+
+    
+
+
 if __name__ == "__main__": #wanna implement the python script if we are running this file directly
     args = sys.argv
 
