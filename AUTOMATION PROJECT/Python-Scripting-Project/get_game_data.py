8396462c28ee0450f60de86b2d30d3c69c919e90
MartinKMwai <martinkibirumwai@gmail.com>
2024-02-19 05:52:34 +0800

    Setting argument parameters, done

--- AUTOMATION PROJECT/Python-Scripting-Project/get_game_data.py
+++ AUTOMATION PROJECT/Python-Scripting-Project/get_game_data.py
@@ -1,1 +1,21 @@
 #time to automate the living fuck outta this bitch
+
+import os 
+import json #dealinmg with json files
+import shutil #copy and overwrite on files. File operationms here
+from subprocess import PIPE, run #run any terminal commands that we want
+import sys #access to cmd args
+
+if __name__ == "__main__": #wanna implement the python script if we are running this file directly
+    args = sys.argv
+
+    if len(args)!=3: #1. Code file we want to run. 2. Source directory 3. Destination directory  
+        raise Exception("Specify the source target directories only")
+
+    source, target = args[1:] #start at index 1, ignore the file name
+    main(source, target)
+
+
+
+
+
