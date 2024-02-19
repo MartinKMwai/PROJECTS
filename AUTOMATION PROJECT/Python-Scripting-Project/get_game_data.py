561f34a3da9ad0edefd8cfd5844c99375c11afd2
MartinKMwai <martinkibirumwai@gmail.com>
2024-02-20 06:17:36 +0800

    Function to collect all game paths in the source directory is done

--- AUTOMATION PROJECT/Python-Scripting-Project/get_game_data.py
+++ AUTOMATION PROJECT/Python-Scripting-Project/get_game_data.py
@@ -6,14 +6,37 @@ import shutil #copy and overwrite on files. File operationms here
 from subprocess import PIPE, run #run any terminal commands that we want
 import sys #access to cmd args
 
+GAME_DIR_PATTERN ="game" 
+
+
+def find_all_game_paths(source):
+    game_paths = []
+
+    for root, dirs, files in os.walk(source):#we are walking the source path
+        for directory in dirs:
+            if GAME_DIR_PATTERN in directory.lower():
+                path = os.path.join(source, directory)
+                game_paths.append(path)
+                
+        break #we do not need to go deeper than the first level of the source directory.
+
+    return game_paths    
+
+   
+
 def main(source, target):
     current_working_directory = os.getcwd()
     source_path = os.path.join(current_working_directory, source)
     #do not use string concatenations since rthe path dividwers are different depending on the os
     target_path = os.path.join(current_working_directory, target)
+    path_to_games = find_all_game_paths(source_path)
 
-    
+    print(path_to_games)
 
+
+
+'''We are using the function below so that this only runs when we are running the script itself, 
+rather than when we are importing functions from here to other files'''
 
 if __name__ == "__main__": #wanna implement the python script if we are running this file directly
     args = sys.argv
