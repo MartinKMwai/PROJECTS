449e8ca27ca1f8c9806150078894c0c0e4b0226e
MartinKMwai <martinkibirumwai@gmail.com>
2024-04-10 05:59:15 +0800

    Path parameters implementation using inventory example

--- FAST API/workingfile.py
+++ FAST API/workingfile.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-@app.get("/")
 
-def home():
-    return {"Data":"Test"}
+# @app.get("/")
 
-@app.get("/about")
-def about():
-    return {"Data":"About"}
+# def home():
+#     return {"Data":"Test"}
+
+# @app.get("/about")
+# def about():
+#     return {"Data":"About"}
+
+#Path parameters 
+inventory = {
+    1: {"name": "apple", "price": 100},
+    2: {"name": "yorghurt", "price": 50},
+    3: {"name": "apple juice", "price": 30},
+    4: {"name": " luncheon meat", "price": 50},
+    5: {"name": "mince beef", "price": 60}
+}
+
+@app.get("/get-item/{item_id}")
+def get_item(item_id: int):
+    return inventory[item_id]
