7ce575c35fc6c428254fa1170e63d4f1785e3487
MartinKMwai <martinkibirumwai@gmail.com>
2024-04-11 06:27:08 +0800

    Prettifying

--- FAST API/workingfile.py
+++ FAST API/workingfile.py
@@ -13,11 +13,11 @@ app = FastAPI()
 
 #Path parameters 
 inventory = {
-    1: {"name": "apple", "price": 100},
-    2: {"name": "yorghurt", "price": 50},
-    3: {"name": "apple juice", "price": 30},
-    4: {"name": " luncheon meat", "price": 50},
-    5: {"name": "mince beef", "price": 60}
+    1: {"name": "Apple", "price": 100},
+    2: {"name": "Yoghurt", "price": 50},
+    3: {"name": "Apple Juice", "price": 30},
+    4: {"name": "Macadamia Nuts", "price": 50},
+    5: {"name": "Coffee", "price": 60}
 }
 
 @app.get("/get-item/{item_id}")
