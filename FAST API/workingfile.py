41077b3161c18f60957d78bee098a1012f4ae473
MartinKMwai <martinkibirumwai@gmail.com>
2024-04-16 06:45:16 +0800

    Post Method, id requirement prompt still pending

--- FAST API/workingfile.py
+++ FAST API/workingfile.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Query
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    brand: Optional[str] = None
 
 # @app.get("/")
 
@@ -13,21 +19,26 @@ app = FastAPI()
 #     return {"Data":"About"}
 
 #Path parameters 
-inventory = {
-    1: {"name": "Apple", "price": 100},
-    2: {"name": "Yoghurt", "price": 50},
-    3: {"name": "Apple Juice", "price": 30},
-    4: {"name": "Macadamia Nuts", "price": 50},
-    5: {"name": "Coffee", "price": 60}
-}
+inventory = {}
 
 @app.get("/get-item/{item_id}")
 def get_item(item_id: int = Path(description = "The ID of the item you'd like to view", gt = 0, le = 5)):
     return inventory[item_id]
 
 @app.get("/get-by-name")
-def get_name(*, name: Optional [str] = None, test: int):
+def get_name(*, name: str = Query(None, title = "Name", description = "Name of Item", max_length = 8, min_length = 2)):
     for item_id in inventory:
-        if inventory[item_id]["name"] == name:
+        if inventory[item_id].name== name:
             return inventory[item_id]
-    return {"Error":"Data not Found" } 
+    return {"Error":"Data not Found"}
+
+#post request
+@app.post("/create-item/{item_id}")
+def create_item(item_id: int, item: Item):
+    if item_id in inventory:
+        return {" Redundant": f"Item with id {item_id} already exists"}
+    inventory[item_id] = Item
+    return inventory[item_id]
+
+
+
