a07964682561bfe98c8b328e0e2311498b15f545
MartinKMwai <martinkibirumwai@gmail.com>
2025-09-20 12:10:20 +0800

    Test

---
+++ SCRIPTING_NETWORKS/email_automation.py
@@ -0,0 +1,15 @@
+import os 
+import requests
+import smtplib
+
+wed_url = "https://phet-dev.colorado.edu/html/build-an-atom/0.0.0-3/simple-text-only-test-page.html"
+recipient_email = "mwaimartinkibiru10721@gmail.com"
+sender_email = "martinkibirumwai@gmail.com"
+smtp_server = "smtp.gmail.com"
+
+#send request to website
+#put it in an email
+#send the email
+
+
+
